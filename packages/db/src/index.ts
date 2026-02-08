8f85f01dd411b276a0e221ce6cdf3ab2f1b698f0
Chandan chaudhary <chandanchaudhary3949@gmail.com>
2026-02-08 09:18:13 +0530

    updated db: tsconfig, package, index.ts; hub: package; added root: tsconfig

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,3 @@
 export * from "@prisma/client";
-export { prisma } from "./client";
+export { prisma } from "./client.js";
 // export { PrismaClient, InvoiceStatus, LedgerType } from "@prisma/client";
